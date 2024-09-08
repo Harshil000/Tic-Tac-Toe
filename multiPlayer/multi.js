7f4ee9fa92775c0ae8940a8d507cbe097e21f5d0
Harshil000 <techshock01@gmail.com>
2024-09-08 16:52:33 +0530

    Just added changed buttons for choosing who will play first in multiPlayer Game

--- multiPlayer/multi.js
+++ multiPlayer/multi.js
@@ -1,2 +1,10 @@
-// document.getElementById("P1Label").innerHTML = document.getElementById("Player1").value
-// console.log(document.body.getElementById("Player1"))
+document.getElementById("P1Label").innerHTML = document.getElementById("Player1").value
+document.getElementById("P2Label").innerHTML = document.getElementById("Player2").value
+
+const ChangePlayersName = () => {
+    document.getElementById("P1Label").innerHTML = document.getElementById("Player1").value
+    document.getElementById("P2Label").innerHTML = document.getElementById("Player2").value
+}
+
+let playFirst = document.getElementById("P1Label")
+console.log(playFirst)
